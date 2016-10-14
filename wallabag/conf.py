492080594a64523a891bc73e3b277a45cccb6bf8
nepochal <development@mischolz.de>
2016-10-14 18:53:38 +0200

    Implemented loading the config file
    Added a bunch of other functionality to conf

--- wallabag/conf.py
+++ wallabag/conf.py
@@ -12,11 +12,23 @@ class Configs():
     Static struct for storing the global configs.
     """
 
+    # wallabag server
     serverurl = ""
     username = ""
     password = ""
-    oa2client = ""
-    oa2secret = ""
+
+    # wallabag api oauth2
+    client = ""
+    secret = ""
+
+
+def set_config(name, value):
+    if hasattr(Configs, name):
+        setattr(Configs, name, value)
+
+
+def get_config(name):
+    return getattr(Configs, name, None)
 
 
 def __configs2dictionary():
@@ -25,8 +37,8 @@ def __configs2dictionary():
 
     Returns
     -------
-    bool
-    Dictionary with the configurations
+    dictionary
+        Dictionary with the configurations
     """
 
     wallabag_api = OrderedDict()
@@ -35,17 +47,64 @@ def __configs2dictionary():
     wallabag_api['serverurl'] = Configs.serverurl
     wallabag_api['username'] = Configs.username
     wallabag_api['password'] = Configs.password
-    wallabag_api_oauth2['client'] = Configs.oa2client
-    wallabag_api_oauth2['secret'] = Configs.oa2secret
+    wallabag_api_oauth2['client'] = Configs.client
+    wallabag_api_oauth2['secret'] = Configs.secret
     wallabag_api["oauth2"] = wallabag_api_oauth2
 
     return {"wallabag_api": wallabag_api}
 
 
+def __dicionary2config(configdict):
+    for item in configdict:
+        if type(item) is str:
+            set_config(item, configdict[item])
+        elif type(item) is dict:
+            __dicionary2config(item)
+
+
 def save(path=CONFIG_FILENAME):
+    """
+    Saves the config into a file.
+
+    Parameters
+    ----------
+    path : string
+        Optional non default config filename.
+
+    Returns
+    -------
+    bool
+        True if successful
+    """
     file = open(path, mode='w')
 
     jsonsave = json.dumps(__configs2dictionary(), indent=4)
     file.write(jsonsave)
 
     file.close()
+    return True
+
+
+def load(path=CONFIG_FILENAME):
+    """
+    Loads the config into a dictionary.
+
+    Parameters
+    ----------
+    path : string
+        Optional non default config filename.
+
+    Returns
+    -------
+    bool
+        True if successfull. Otherwise the config will be filles with default values
+    """
+    file = open(path, mode='r')
+
+    filecontent = file.read()
+    file.close()
+    dic = json.loads(filecontent)
+    __dicionary2config(dic['wallabag_api'])
+    return True
+
+load()
