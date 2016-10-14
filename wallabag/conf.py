9870670160c37307091a2737c67324e7487a3191
nepochal <development@mischolz.de>
2016-10-14 12:28:54 +0200

    Added config to dictionary converter
    Added dummy config save method

--- wallabag/conf.py
+++ wallabag/conf.py
@@ -1,16 +1,44 @@
 """
 Settings and configuration for wallabag-api.
 """
+import json
+from collections import OrderedDict
+
+CONFIG_FILENAME = ".wallabag-api"
 
 
-class Configs:
+class Configs():
     """
     Static struct for storing the global configs.
     """
-    CONFIG_FILENAME = ".wallabag-api"
 
-    url = ""
+    serverurl = ""
     username = ""
     password = ""
     oa2client = ""
     oa2secret = ""
+
+
+def __configs2dictionary():
+    """
+    Converts the configuration values to a json serializable dictionary.
+
+    Return: Dictionary
+    """
+
+    wallabag_api = OrderedDict()
+    wallabag_api_oauth2 = OrderedDict()
+
+    wallabag_api['serverurl'] = Configs.serverurl
+    wallabag_api['username'] = Configs.username
+    wallabag_api['password'] = Configs.password
+    wallabag_api_oauth2['client'] = Configs.oa2client
+    wallabag_api_oauth2['secret'] = Configs.oa2secret
+    wallabag_api["oauth2"] = wallabag_api_oauth2
+
+    return {"wallabag_api": wallabag_api}
+
+
+def save(path=CONFIG_FILENAME):
+    jsonsave = json.dumps(__configs2dictionary(), indent=4)
+    print(jsonsave)
