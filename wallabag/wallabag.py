ca76faea11de4f181e8e661ca74a4be330f36b2d
nepochal <development@mischolz.de>
2016-10-15 00:17:35 +0200

    Implemented opening the help and analyzing the args in the main module

--- wallabag/wallabag.py
+++ wallabag/wallabag.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
-import conf
-import wallabag_help
 
-wallabag_help.show(__file__)
+from sys import argv
+from wallabag_help import show as help
+
+command = None
+show_help = False
+
+# Determine command or general standalone option
+if len(argv) == 1 or argv[1] in {'-h', '--help'}:
+    help(argv[0])
+    exit(0)
+elif not argv[1][0] == '-':
+    print("Invalid command \"{0}\".".format(argv[1]))
+    print("Use \"{0}\" to see a full list of commands.".format(argv[0]))
+    exit(-1)
+else:
+    print("Invalid option \"{0}\".".format(argv[1]))
+    print("Use \"{0}\" to see a full list of options.".format(argv[0]))
+    exit(-1)
