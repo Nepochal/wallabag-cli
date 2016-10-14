82f04ff281c145ad91225d66bddcd39057d6d4ad
nepochal <development@mischolz.de>
2016-10-14 09:50:27 +0200

    Added config file with a struct for storing global configs

---
+++ wallabag/config/config.py
@@ -0,0 +1,11 @@
+class config:
+    """
+    Static struct for storing the global configs.
+    """
+    CONFIG_FILENAME = ".wallabag-api"
+
+    url = ""
+    username = ""
+    password = ""
+    oa2client = ""
+    oa2secret = ""
