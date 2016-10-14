37c0590ac07ee71375aaa534a4a1a9a6468fcbcb
nepochal <development@mischolz.de>
2016-10-15 00:51:58 +0200

    Added check for invalid options in the config module and starting the help for config

--- wallabag/wallabag.py
+++ wallabag/wallabag.py
@@ -4,8 +4,17 @@ from sys import argv
 from wallabag_help import show as help
 import wallabag_config
 
+def checkCommands(command, options, allowed_options):
+    for option in options:
+        if not option in allowed_options:
+            print("Invalid option {0} in command {1}.".format(option, command))
+            print("Use {0} {1} --help for a list of allowed options.".format(argv[0], command))
+            print()
+            return False
+    return True
+
+
 command = None
-show_help = False
 
 # Determine command or general standalone option
 if len(argv) == 1 or argv[1] in {'-h', '--help'}:
@@ -22,5 +31,13 @@ else:
     print("Use \"{0}\" to see a full list of options.".format(argv[0]))
     exit(-1)
 
+optionlist = argv[2:len(argv)]
+
 if command == "config":
+    allowed_options = [ ]
+    if "-h" in optionlist or "--help" in optionlist:
+        help(argv[0], command)
+        exit(0)
+    if not checkCommands(command, optionlist, allowed_options):
+        exit(-1)
     wallabag_config.start()
