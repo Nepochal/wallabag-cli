b49a2f95b61d0c0499dcebcb224c62b8e369b711
nepochal <development@mischolz.de>
2016-10-15 17:50:40 +0200

    Forced to insert values in the initial configuration

--- wallabag/wallabag_config.py
+++ wallabag/wallabag_config.py
@@ -44,46 +44,85 @@ def start(ask_serverurl=True, ask_username=True, ask_password=True, ask_oauth2=T
 
 
 def __serverurl(forced):
+    error = False
     print()
     print("Enter the url of your Wallabag instance.")
     print("e.g. https://www.wallabag.com")
     if not forced:
         print("(Leave the text empty to leave the url unchanged.)")
     value = input()
+
+    if forced and value == "":
+        error = True
+
+    if error:
+        return __serverurl(forced)
     return value
 
 
 def __username(forced):
+    error = False
     print()
     print("Enter your Wallabag username.")
     if not forced:
         print("(Leave the text empty to leave the username unchanged.)")
     value = input()
+
+    if forced and value == "":
+        error = True
+
+    if error:
+        return __username(forced)
+
     return value
 
 
 def __password(forced):
+    error = False
     print()
     print("Enter your Wallabag password.")
     if not forced:
         print("(Leave the text empty to leave the password unchanged.)")
     value = input()
+
+    if forced and value == "":
+        error = True
+
+    if error:
+        return __password(forced)
+
     return value
 
 
 def __client(forced):
+    error = False
     print()
     print("Enter the client id of your Wallabag API.")
     if not forced:
         print("(Leave the text empty to not change the client id.)")
     value = input()
+
+    if forced and value == "":
+        error = True
+
+    if error:
+        return __client(forced)
+
     return value
 
 
 def __secret(forced):
+    error = False
     print()
     print("Enter the client secret of your Wallabag API.")
     if not forced:
         print("(Leave the text empty to not change the client secret.)")
     value = input()
+
+    if forced and value == "":
+        error = True
+
+    if error:
+        return __secret(forced)
+
     return value
