2ea53decc5879dd06ed6306ba48adb6c061a2f20
nepochal <development@mischolz.de>
2016-10-16 14:09:36 +0200

    Added check for minimum API version
    Set minimum API version to 2.1.1

--- wallabag/api.py
+++ wallabag/api.py
@@ -2,8 +2,12 @@
 Wallabag API accesses.
 """
 from enum import Enum
+import re
 import requests
 import conf
+
+
+MINIMUM_API_VERSION = 2, 1, 1
 
 
 class Error(Enum):
@@ -28,6 +32,33 @@ def __getApiUrl(api_method):
     if api_method in ApiMethod:
         return conf.get_config('serverurl') + api_method.value
     return None
+
+
+def is_minimum_version(versionstring):
+    if not re.compile('"\\d+\\.\\d+\\.\\d+"').match(versionstring):
+        return False
+
+    ver = versionstring.strip('"').split('.')
+
+    x = int(ver[0])
+    y = int(ver[1])
+    z = int(ver[2])
+    tx, ty, tz = MINIMUM_API_VERSION
+
+    if x > tx:
+        return True
+    elif x < tx:
+        return False
+    else:
+        if y > ty:
+            return True
+        elif y < ty:
+            return False
+        else:
+            if z >= tz:
+                return True
+            else:
+                return False
 
 
 def version():
