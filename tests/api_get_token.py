8656f396159ab15dced976b3e9d3ece3fd467094
nepochal <development@mischolz.de>
2016-10-17 10:15:55 +0200

    Implemented test case for generating access tokens

---
+++ tests/api_get_token.py
@@ -0,0 +1,20 @@
+import context
+import json
+import conf
+import api
+
+conf.load()
+
+response = api.token()
+
+print("HTTP Status: " + str(response.http_code))
+print("Error: " + str(response.error))
+print("Errortext: " + response.error_text)
+print("Errordescription: " + response.error_description)
+print("Body: " + str(response.response))
+
+if response.error == api.Error.ok:
+    print()
+    value = json.loads(response.response)
+    print("Access-token:" + value['access_token'])
+    print("Validity:" + str(value['expires_in']) + "seconds")
