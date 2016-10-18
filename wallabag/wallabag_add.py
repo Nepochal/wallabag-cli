9189f2def7958215e93d86fdf6f77d3d46fed695
nepochal <development@mischolz.de>
2016-10-18 13:48:39 +0200

    Added module for adding new entries

---
+++ wallabag/wallabag_add.py
@@ -0,0 +1,31 @@
+"""
+Module for adding new entries
+"""
+import re
+import api
+import conf
+
+
+def add(target_url):
+    conf.load()
+
+    valid_url = False
+    if not re.compile("(?i)https?:\\/\\/.+").match(target_url):
+        for protocol in "https://", "http://":
+            if api.is_valid_url("{0}{1}".format(protocol, target_url)):
+                target_url = "{0}{1}".format(protocol, target_url)
+                valid_url = True
+                break
+    else:
+        valid_url = api.is_valid_url(target_url)
+
+    if not valid_url:
+        print("Error: Invalid url to add.")
+        exit(-1)
+
+    request = api.api_add_entry(target_url)
+    if(request.hasError()):
+        print("Error")
+    else:
+        print("Entry successfully added")
+        exit(0)
