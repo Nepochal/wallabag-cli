3ab3ad709ba38a818af5e35810f93e0551962b58
nepochal <development@mischolz.de>
2016-10-18 11:55:44 +0200

    Added test file for adding new entries

---
+++ tests/api_post_entry.py
@@ -0,0 +1,17 @@
+import context
+import json
+import conf
+import api
+
+NEW_ENTRY = "https://en.wikipedia.org/wiki/Bill_%26_Ted%27s_Excellent_Adventure"
+CUSTOM_TITLE = "Bogus!"
+
+conf.load()
+
+response = api.api_add_entry(NEW_ENTRY)
+
+print("HTTP Status: " + str(response.http_code))
+print("Error: " + str(response.error))
+print("Errortext: " + response.error_text)
+print("Errordescription: " + response.error_description)
+print("Body: " + str(response.response))
