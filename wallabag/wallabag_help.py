86562a83713d9b2b00d400ca83b2f2c8c6c30206
nepochal <development@mischolz.de>
2016-11-03 10:15:40 +0100

    Added the "--config"-parameter to the internal help system

--- wallabag/wallabag_help.py
+++ wallabag/wallabag_help.py
@@ -18,19 +18,20 @@ Usage:
   {0} <command> [options]
 
 Commands:
-  config         Start the configuration.
-  add            Add a new entry to wallabag.
-  delete         Delete an entry.
-  list           List the entries on the wallabag account.
-  update         Change the read- or starred-status of an existing entry
-                 or change the title.
-  read           Toggle the read-status of an entry.
-  star           Toggle the starred-status of an entry.
+  config           Start the configuration.
+  add              Add a new entry to wallabag.
+  delete           Delete an entry.
+  list             List the entries on the wallabag account.
+  update           Change the read- or starred-status of an existing entry
+                   or change the title.
+  read             Toggle the read-status of an entry.
+  star             Toggle the starred-status of an entry.
 
 General Options:
-  -h, --help     Show help.
-  --about        Show about information.
-  -v, --version  Show version No.
+  -h, --help       Show help.
+  --config=<path>  Use a custom config file.
+  --about          Show about information.
+  -v, --version    Show version No.
 
 Use \"{0} <command> --help\" for further information.
 And don't forget to be excellent to each other!
@@ -46,10 +47,11 @@ Description:
   Start the configuration.
 
 Options:
-  -h, --help      Show help
-  -c, --check     Check the config for errors
-  -p, --password  Change the wallabag password
-  -o, --oauth     Change the wallabag client credentials
+  -h, --help       Show help.
+  -c, --check      Check the config for errors.
+  -p, --password   Change the wallabag password.
+  -o, --oauth      Change the wallabag client credentials.
+  --config=<path>  Use a custom config file.
 """.format(startscript)
     index['config'] = config_txt
 
@@ -62,10 +64,11 @@ Description:
   Add a new link to wallabag.
 
 Options:
-  -h, --help           Show help
-  -t, --title <title>  Add a custom title
-  -s, --starred        Mark as starred
-  -r, --read           Mark as read
+  -h, --help           Show help.
+  -t, --title <title>  Add a custom title.
+  -s, --starred        Mark as starred.
+  -r, --read           Mark as read.
+  --config=<path>      Use a custom config file.
 """.format(startscript)
     index['add'] = add_txt
 
@@ -79,8 +82,9 @@ Description:
   The entry-id can be found in "{0} list" for example.
 
 Options:
-  -h, --help   Show help
-  -f, --force  Do not ask before deletion
+  -h, --help       Show help.
+  -f, --force      Do not ask before deletion.
+  --config=<path>  Use a custom config file.
 """.format(startscript)
     index['delete'] = delete_txt
 
@@ -93,17 +97,18 @@ Description:
   Gives a summary of entries in wallabag. Use options to filter the results.
 
 Options:
-  -h, --help                     Show help
-  -q, --quantity <"all"|number>  Set the number of entries to show
-  --unread                       Show only unread entries (default)
-  -r, --read                     Show only read entries
-  -a, --all                      Show read as well as unread entries
-  -s, --starred                  Show only starred entries
-  -u, --unstarred                Show only unstarred entries
-  -o, --oldest                   Show oldest matches instead of the newest
+  -h, --help                     Show help.
+  -q, --quantity <"all"|number>  Set the number of entries to show.
+  --unread                       Show only unread entries (default).
+  -r, --read                     Show only read entries.
+  -a, --all                      Show read as well as unread entries.
+  -s, --starred                  Show only starred entries.
+  -u, --unstarred                Show only unstarred entries.
+  -o, --oldest                   Show oldest matches instead of the newest.
   -f, --full                     Do not trim the titles to fit the length
-                                 of the cli
-  -c, --count                    Show a sum of matching entries
+                                 of the cli.
+  -c, --count                    Show a sum of matching entries.
+  --config=<path>                Use a custom config file.
 """.format(startscript)
     index['list'] = list_txt
 
@@ -117,10 +122,11 @@ Description:
   The entry-id can be found in "{0} list" for example.
 
 Options:
-  -h, --help             Show help
-  -r, --read             Toggle the read-status
-  -s, --starred          Toggle the starred-status
-  -t, --title <"title">  Change the title
+  -h, --help             Show help.
+  -r, --read             Toggle the read-status.
+  -s, --starred          Toggle the starred-status.
+  -t, --title <"title">  Change the title.
+  --config=<path>        Use a custom config file.
 """.format(startscript)
     index['update'] = update_txt
 
@@ -135,7 +141,8 @@ Description:
   The entry-id can be found in "{0} list" for example.
 
 Options:
-  -h, --help             Show help
+  -h, --help       Show help.
+  --config=<path>  Use a custom config file.
 """.format(startscript)
     index['read'] = read_txt
 
@@ -150,7 +157,8 @@ Description:
   The entry-id can be found in "{0} list" for example.
 
 Options:
-  -h, --help             Show help
+  -h, --help       Show help.
+  --config=<path>  Use a custom config file.
 """.format(startscript)
     index['star'] = star_txt
 
